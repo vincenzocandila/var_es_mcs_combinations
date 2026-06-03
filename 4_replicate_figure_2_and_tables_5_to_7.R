4da5b22d51a98e572e27a5a67e3444319fab2e27
Vincenzo Candila <vcandila@unisa.it>
2026-06-03 15:39:23 +0200

    Add files via upload

--- 4_replicate_figure_2_and_tables_5_to_7.R
+++ 4_replicate_figure_2_and_tables_5_to_7.R
@@ -11,16 +11,15 @@
 ###################################
 
 library(xts)			# Version used: 0.14.1
-library(fBasics)		# Version used: 4041.97
 library(rugarch)		# Version used: 1.5.3
 library(np)     		# Version used: 0.60.18
-library(esback)		# Version used: 0.3.1
+library(esback)			# Version used: 0.3.1
 library(GAS)			# Version used: 0.3.4.1 
-					# Currently, the GAS package has been removed from the CRAN. But previous versions
-					# are available at https://cran.r-project.org/src/contrib/Archive/GAS/
+						# Currently, the GAS package has been removed from the CRAN. But previous versions
+						# are available at https://cran.r-project.org/src/contrib/Archive/GAS/
 library(zoo)			# Version used: 1.8.13
-library(DT)			# Version used: 0.33
-library(htmltools)	# Version used: 0.5.8.1
+library(DT)				# Version used: 0.33
+library(htmltools)		# Version used: 0.5.8.1
 
 source('functions/main_functions.R')
 
