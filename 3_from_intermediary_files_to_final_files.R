e1351e8f264edf148d32afbb21005c4ac29d6b6c
Vincenzo Candila <vcandila@unisa.it>
2026-06-03 15:28:58 +0200

    Add files via upload

--- 3_from_intermediary_files_to_final_files.R
+++ 3_from_intermediary_files_to_final_files.R
@@ -9,15 +9,15 @@
 # 1. Load libraries and functions
 #####################################################################################
 
-library(xts)				# Version used: 0.14.1
-library(fBasics)				# Version used: 4041.97
-library(rugarch)				# Version used: 1.5.3
+library(xts)			# Version used: 0.14.1
+library(fBasics)		# Version used: 4041.97
+library(rugarch)		# Version used: 1.5.3
 library(np)				# Version used: 0.60.18
 library(esback)			# Version used: 0.3.1
-library(GAS)				# Version used: 0.3.4.1 
+library(GAS)			# Version used: 0.3.4.1 
 						# Currently, the GAS package has been removed from the CRAN. But previous versions
 						# are available at https://cran.r-project.org/src/contrib/Archive/GAS/
-library(zoo)				# Version used: 1.8.13
+library(zoo)			# Version used: 1.8.13
 library(DT)				# Version used: 0.33
 library(htmltools)		# Version used: 0.5.8.1
 
